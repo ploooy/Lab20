485a77ae2086ec34fb3ee643372c14b14bf82642
somebody <somebody@example.com>
2022-01-31 19:44:40 +0300

    Added all scripts

---
+++ tabspc2.sql
@@ -0,0 +1,5 @@
+CREATE TABLESPACE space1 OWNER "usershah484" LOCATION '/home/user/space1';
+
+CREATE TABLESPACE space2 OWNER "usershah484" LOCATION '/home/user/space2';
+
+CREATE TABLESPACE space3 OWNER "usershah484" LOCATION '/home/user/space3';
